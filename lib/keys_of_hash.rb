d9e170ddb166d099edb7d0683e45b233a53e98d3
Jane Costa <yberdysh@gmail.com>
2018-06-24 19:44:21 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
     []
-    animals = self.keys.select{|key| self[key] == arguments}
-    binding.pry
+    animals = self.keys.select{|key| arguments.include?(self[key])}
+    # binding.pry
   end
 end
